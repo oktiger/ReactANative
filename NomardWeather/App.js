ed898556dd89dfd019f8546e91c2d52d2abaecc2
oksehun <sehun9803@naver.com>
2024-03-04 14:31:28 +0900

    0304

---
+++ NomardWeather/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>뤼승집이</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 180,
+    color: 'red',
+  }
+});
