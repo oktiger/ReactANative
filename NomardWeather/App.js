fc7670714294173875ab00d3ea9628e6f2232527
oksehun <sehun9803@naver.com>
2024-03-08 17:53:42 +0900

    Loction: 8:08

--- NomardWeather/App.js
+++ NomardWeather/App.js
@@ -1,15 +1,83 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Dimensions, Text, View, ScrollView } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width
 
 export default function App() {
-  return (<View style={styles.container}></View>
+  const [location, setLocation] = useState()
+  const [ok, setOk] = useState(true)
+  const ask = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false)
+    }
+    const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync({ accuracy: 5 })
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    )
+    console.log(location)
+  }
+  useEffect(() => {
+    ask()
+  }, [])
+  return (<View style={styles.container}>
+    <View style={styles.city}>
+      <Text style={styles.cityName}>Seoul</Text>
+    </View>
+    <ScrollView
+      horizontal
+      pagingEnabled
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={styles.weather}
+    >
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+      <View style={styles.day}>
+        <Text style={styles.temp}>27</Text>
+        <Text style={styles.description}>Sunny</Text>
+      </View>
+    </ScrollView>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, backgroundColor: "tomato"
-  }
+    flex: 1, backgroundColor: "teal"
+  },
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500'
+  },
+  weather: {
+
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
 })
 
 
