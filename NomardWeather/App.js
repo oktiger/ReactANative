7f91b49b1a367f553599e8528035c7c54d2181fe
oksehun <sehun9803@naver.com>
2024-03-15 17:31:09 +0900

    API 오류 발생 - 무료API를 사용해야할 듯

--- NomardWeather/App.js
+++ NomardWeather/App.js
@@ -2,11 +2,12 @@ import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
 import { StyleSheet, Dimensions, Text, View, ScrollView } from 'react-native';
-
 const SCREEN_WIDTH = Dimensions.get('window').width
+const API_KEY = "233f757f83aa38276611bcc15cb7184d"
 
 export default function App() {
-  const [location, setLocation] = useState()
+  const [city, setCity] = useState("Loading....")
+  const [days, setDays] = useState([])
   const [ok, setOk] = useState(true)
   const ask = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
@@ -18,14 +19,17 @@ export default function App() {
       { latitude, longitude },
       { useGoogleMaps: false }
     )
-    console.log(location)
+    setCity(location[0].city)
+    const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${API_KEY}`)
+    const json = await response.json();
+    console.log(json)
   }
   useEffect(() => {
     ask()
   }, [])
   return (<View style={styles.container}>
     <View style={styles.city}>
-      <Text style={styles.cityName}>Seoul</Text>
+      <Text style={styles.cityName}>{city}</Text>
     </View>
     <ScrollView
       horizontal
